754bc891ca7810b8312d0ff1cc0eb0f6081001d4
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-10-31 15:26:59 +0100

    MAIN: Creazione delle cards

--- laravel-comics/resources/views/shared/areaHomeTop.blade.php
+++ laravel-comics/resources/views/shared/areaHomeTop.blade.php
@@ -1,3 +1,29 @@
 <div class="container">
-    MAIN SUPERIORE
+    <div class="cards">
+        <h2>CURRENT SERIES</h2>
+        @foreach ($seriesComics as $item)
+            <div class="card">
+                <img class="imageCard" src="{{ asset($item['thumb']) }}" alt="{{ $item['series'] }}">
+                <h3>{{ $item['series'] }}</h3>
+            </div>
+        @endforeach
+        <input type="button" value="LOAD MORE">
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </div>
